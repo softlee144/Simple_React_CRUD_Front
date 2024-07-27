607a237b9401269027032134b89ca8930adf573e
softlee144 <ockjungbong@gmail.com>
2024-07-28 01:11:59 +0900

    글 목록 보기 완료

--- src/pages/book/Home.js
+++ src/pages/book/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BookItem from '../../components/BookItem';
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  // 함수 실행 시 최초 한 번 실행되는 것 + deps [] 처리 안 해주면 상태 변경될 때마다 실행
+  useEffect(() => {
+    fetch('http://localhost:8080/book')
+      .then((res) => res.json()) // 응답이 오면 응답 결과를 json으로 변경시킨다.
+      .then((res) => {
+        console.log(1, res);
+        setBooks(res);
+      }); // 비동기 함수
+  }, []);
+
   return (
     <div>
-      <h1>책 리스트 보기</h1>
+      {books.map((book) => (
+        <BookItem key={book.id} book={book} />
+      ))}
     </div>
   );
 };
