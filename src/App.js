3acb635bad97da7a01b85e9c44400f81df5f9e00
softlee144 <ockjungbong@gmail.com>
2024-07-27 22:27:54 +0900

    화면 구성하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Home from './pages/book/Home';
+import SaveForm from './pages/book/SaveForm';
+import Detail from './pages/book/Detail';
+import LoginForm from './pages/user/LoginForm';
+import JoinForm from './pages/user/JoinForm';
+import UpdateForm from './pages/book/UpdateForm';
 
+// 화면 구성하기
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" exact={true} Component={Home} />
+          <Route path="/saveForm" exact={true} Component={SaveForm} />
+          <Route path="/book/:id" exact={true} Component={Detail} />
+          <Route path="/loginForm" exact={true} Component={LoginForm} />
+          <Route path="/joinForm" exact={true} Component={JoinForm} />
+          <Route path="/updateForm/:id" exact={true} Component={UpdateForm} />
+        </Routes>
+      </Container>
     </div>
   );
 }
