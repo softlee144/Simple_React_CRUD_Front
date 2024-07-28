5536b45bcc7037cc385e3ecbe2d0f0faff8e5c6c
softlee144 <ockjungbong@gmail.com>
2024-07-28 13:30:32 +0900

    글쓰기 완료

--- src/pages/book/SaveForm.js
+++ src/pages/book/SaveForm.js
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
-const SaveForm = () => {
+const SaveForm = (props) => {
+  const [book, setBook] = useState({
+    title: '',
+    authour: '',
+  });
+
+  const changeValue = (e) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value, // compute property names
+    });
+  };
+
+  const submitBook = (e) => {
+    e.preventDefault(); // submit이 action을 안 타고 자기 할 일을 중지함.
+
+    fetch('http://localhost:8080/book', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify(book),
+    })
+      .then((res) => {
+        if (res.status === 201) {
+          return res.json();
+        } else {
+          return null;
+        }
+      })
+      .then((res) => {
+        if (res !== null) {
+          //props.history.push('/'); 이거 왜 안돼지??
+          console.log('책 등록 성공');
+        } else {
+          alert('책 등록에 실패하였습니다.');
+        }
+      });
+  };
+
   return (
-    <div>
-      <h1>책 등록하기</h1>
-    </div>
+    <Form onSubmit={submitBook}>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Title"
+          onChange={changeValue}
+          name="title"
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Author</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Author"
+          onChange={changeValue}
+          name="author"
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
 
